ed858059cfb0b6fb73379281a8e43539b8d50eb1
Malik <permalik@protonmail.com>
2022-08-30 17:53:45 -0500

    Complete initial IP render

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,9 @@
 <template>
   <main>
-    <h1>
-      IP Address Tracker
-    </h1>
-    <input
-      placeholder="Input IP Address"
-      type="text"
-      v-model="findIP"
-    />
+    <h1>IP Address Tracker</h1>
+    <input placeholder="Input IP Address" type="text" v-model="findIP" />
     <button @click="getInfo">X</button>
-    <Info
-      v-bind:info="info"
-      v-if="info"
-    />
+    <Info v-bind:info="info" v-if="info" />
   </main>
   <div>
     <div id="ipMap"></div>
@@ -32,27 +23,73 @@ export default {
     let mapComponent;
     let marker;
     let popup;
+    let initialLat;
+    let initialLng;
+    let initialIP;
+    let initialInfo;
     const findIP = ref("");
     const info = ref(null);
     const tileURL = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-    const attribution = "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors";
-    onMounted(() => {
-      mapComponent = leaflet.map("ipMap").setView([37.3230, -122.0322], 10);
-      marker = leaflet.marker([37.3230, -122.0322]).addTo(mapComponent);
-      popup = leaflet.popup().setLatLng([37.3230, -122.0322]).setContent('Cupertino, California');
+    const attribution =
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+    const getInitialIP = async () => {
+      try {
+        const ipData = await axios.get("https://api.ipify.org?format=json");
+        initialIP = ipData.data.ip;
+        console.log(initialIP);
+        return initialIP;
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    const getInitialLocation = async () => {
+      let ip;
+      ip = await getInitialIP();
+      try {
+        const data = await axios.get(
+          `https://geo.ipify.org/api/v2/country,city?apiKey=at_Qj8nFi0OaT5ACiI2ySdWdTdl3ULkk&ipAddress=${ip}`
+        );
+        console.log(data.data);
+        let result = data.data;
+        initialInfo = {
+          lat: result.location.lat,
+          lng: result.location.lng
+        };
+        initialLat = initialInfo.lat;
+        initialLng = initialInfo.lng;
+        let location = {
+          lat: initialLat,
+          lng: initialLng
+        };
+        console.log(initialLat);
+        console.log(initialLng);
+        return location;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    onMounted(async () => {
+      let initialLocation = await getInitialLocation();
+      let lat, lng;
+      lat = initialLocation.lat;
+      lng = initialLocation.lng;
+      mapComponent = leaflet.map("ipMap").setView([lat, lng], 10);
+      marker = leaflet.marker([lat, lng]).addTo(mapComponent);
+      popup = leaflet
+        .popup()
+        .setLatLng([lat, lng])
+        .setContent("Cupertino, California");
       marker.bindPopup(popup);
-      marker.on("mouseover", function(event) {
+      marker.on("mouseover", function() {
         marker.openPopup();
       });
-      marker.on("mouseout", function(event) {
+      marker.on("mouseout", function() {
         marker.closePopup();
       });
-      leaflet.tileLayer(
-        tileURL,
-        {
+      leaflet
+        .tileLayer(tileURL, {
           attribution: attribution
-        }
-      )
+        })
         .addTo(mapComponent);
     });
 
